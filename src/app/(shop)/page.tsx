28cf9d258c98617fc6385b3f76e9c04dd1b7602d
LostoVayne <moises.barrera.dev@gmail.com>
2024-01-17 19:47:13 -0300

    Agregada la data desde la bd

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,8 +1,9 @@
+import { getPaginateProductsWithImages } from "@/actions";
 import { ProductGrid, Title } from "@/components";
-import { initialData } from "@/seed/seed";
 
-const products = initialData.products;
-export default function Home() {
+export default async function Home() {
+    const { products } = await getPaginateProductsWithImages();
+    console.log(products);
     return (
         <>
             <Title title="Tienda" subtitle="Todos los productos" />
