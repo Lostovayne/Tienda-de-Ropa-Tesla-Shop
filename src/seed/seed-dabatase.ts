6c3f148d9afcbce9f4b51f83eb69793cb66b2671
LostoVayne <moises.barrera.dev@gmail.com>
2024-01-17 00:54:24 -0300

    Agregada la seed a la bd

--- src/seed/seed-dabatase.ts
+++ src/seed/seed-dabatase.ts
@@ -3,6 +3,10 @@ import prisma from "../lib/prisma";
 
 async function main() {
     // Borrar Registros previos
+
+    //  await prisma.productImage.deleteMany();
+    //  await prisma.product.deleteMany();
+    //  await prisma.category.deleteMany();
     await Promise.all([
         await prisma.productImage.deleteMany(),
         await prisma.product.deleteMany(),
@@ -22,17 +26,44 @@ async function main() {
     });
 
     // Tomar los ID de las categorias de la BD
-
     const categoriesDb = await prisma.category.findMany();
 
-    // crear un Mapa
-
+    // crear un Mapa para el id de la categoria
     const categoriesMap = categoriesDb.reduce((map, category) => {
         map[category.name.toLocaleLowerCase()] = category.id;
         return map;
     }, {} as Record<string, string>); // el primer string seria el texto y el segundo es el id
 
-    console.log(categoriesMap);
+    // Insertar Productos {
+    const { images, type, ...product1 } = products[0];
+    // await prisma.product.create({
+    //     data: {
+    //         ...product1,
+    //         categoryId: categoriesMap[type],
+    //     },
+    // });
+
+    products.forEach(async (product) => {
+        const { images, type, ...rest } = product;
+        const dbProduct = await prisma.product.create({
+            data: {
+                ...rest,
+                categoryId: categoriesMap[type],
+            },
+        });
+
+        // Insertar Images
+        const imagesData = images.map((image) => {
+            return {
+                url: image,
+                productId: dbProduct.id,
+            };
+        });
+
+        await prisma.productImage.createMany({
+            data: imagesData,
+        });
+    });
 
     console.log("Seed ejecutado correctamente");
 }
