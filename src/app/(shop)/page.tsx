2aaf4faecbb964e01cdc58a4931517766a124146
LostoVayne <moises.barrera.dev@gmail.com>
2024-01-18 19:53:01 -0300

    update page

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -3,7 +3,6 @@ import { ProductGrid, Title } from "@/components";
 
 export default async function Home() {
     const { products } = await getPaginateProductsWithImages();
-    console.log(products);
     return (
         <>
             <Title title="Tienda" subtitle="Todos los productos" />
