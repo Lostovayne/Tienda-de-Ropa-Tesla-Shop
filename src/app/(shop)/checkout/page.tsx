b0fbf1b482d7a96a4689cda645cb161b96ceab75
Deus lo Vult <92962731+Lostovayne@users.noreply.github.com>
2024-01-19 17:54:22 -0300

    updated

--- src/app/(shop)/checkout/page.tsx
+++ src/app/(shop)/checkout/page.tsx
@@ -7,12 +7,12 @@ const productsInCart = [initialData.products[0], initialData.products[1], initia
 
 export default function CheckoutPage() {
     return (
-        <div className="flex justify-center items-center mb-72 px-1 sm:px-0 ">
-            <div className="flex flex-col w-full  xl:w-[1000px] ">
+        <div className="mb-72 flex items-center justify-center px-1 sm:px-0">
+            <div className="flex w-full flex-col xl:w-[1000px]">
                 <Title title="Verificar orden" />
-                <div className="grid grid-cols-1 sm:grid-cols-2 gap-10">
+                <div className="grid grid-cols-1 gap-10 sm:grid-cols-2">
                     {/* Carrito */}
-                    <div className="flex flex-col mt-5">
+                    <div className="mt-5 flex flex-col">
                         {/* <span className="text-xl"> Ajustar elementos </span> */}
                         <Link href="/cart" className="mb-5 underline">
                             Editar carrito
@@ -21,13 +21,13 @@ export default function CheckoutPage() {
                         {/* Items */}
 
                         {productsInCart.map((product) => (
-                            <div key={product.slug} className="flex mb-5 xl:mb-3">
+                            <div key={product.slug} className="mb-5 flex xl:mb-3">
                                 <Image
                                     src={`/products/${product.images[0]}`}
                                     alt={product.title}
                                     width={100}
                                     height={100}
-                                    className="mr-5 rounded-xl border object-fill w-[100px] h-[100px]"
+                                    className="mr-5 h-[100px] w-[100px] rounded-xl border object-fill"
                                 />
                                 <div>
                                     <p>{product.title}</p>
@@ -40,8 +40,8 @@ export default function CheckoutPage() {
                     {/* Checkout */}
 
                     <section>
-                        <div className="bg-white rounded-xl shadow-lg p-7">
-                            <h2 className="text-xl font-semibold mb-2">Información de envío</h2>
+                        <div className="rounded-xl bg-white p-7 shadow-lg">
+                            <h2 className="mb-2 text-xl font-semibold">Información de envío</h2>
                             <div className="mb-10">
                                 <p>Epsaind Barrera</p>
                                 <p>Av. Siempre viva 123</p>
@@ -53,9 +53,9 @@ export default function CheckoutPage() {
                             </div>
 
                             {/* Divider */}
-                            <div className="mb-10 w-full h-0.5 rounded bg-gray-200"></div>
+                            <div className="mb-10 h-0.5 w-full rounded bg-gray-200"></div>
                             {/* Informacion producto */}
-                            <h2 className="text-xl font-semibold mb-2">Resumen de orden</h2>
+                            <h2 className="mb-2 text-xl font-semibold">Resumen de orden</h2>
                             <div className="grid grid-cols-2">
                                 <span className="text-base font-medium">No. Productos</span>
                                 <span className="text-right text-base">3 artículos</span>
@@ -63,13 +63,13 @@ export default function CheckoutPage() {
                                 <span className="text-base font-medium">Subtotal</span>
                                 <span className="text-right text-base">$ 100</span>
 
-                                <span className=" text-base">Impuestos (15%) </span>
+                                <span className="text-base">Impuestos (15%) </span>
                                 <span className="text-right text-base">$ 100</span>
 
-                                <span className="mt-5 text-xl ">Total:</span>
-                                <span className="mt-5 text-xl text-right ">$ 100</span>
+                                <span className="mt-5 text-xl">Total:</span>
+                                <span className="mt-5 text-right text-xl">$ 100</span>
                             </div>
-                            <div className="mt-5 mb-2 w-full">
+                            <div className="mb-2 mt-5 w-full">
                                 {/* Disclamer */}
                                 <p className="mb-5">
                                     <span className="text-sm">
@@ -84,7 +84,7 @@ export default function CheckoutPage() {
                                     </span>
                                 </p>
 
-                                <Link className="flex btn-primary justify-center" href="/orders/1234">
+                                <Link className="btn-primary flex justify-center" href="/orders/1234">
                                     Realizar orden
                                 </Link>
                             </div>
