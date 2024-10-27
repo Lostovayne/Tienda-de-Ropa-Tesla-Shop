685f88f0885c89daba5cd31ffc24c657acce201f
Lostovayne <moises.barrera.dev@gmail.com>
2024-10-26 22:19:30 -0300

    agrega listado de paises

--- src/app/(shop)/checkout/address/page.tsx
+++ src/app/(shop)/checkout/address/page.tsx
@@ -1,13 +1,20 @@
-import { Title } from "@/components";
-import AddressForm from "./ui/AddressForm";
+import { getCountries } from '@/actions/country/get-countries';
+import { Title } from '@/components';
+import { Country } from '@/interfaces/country.interface';
+import AddressForm from './ui/AddressForm';
 
-export default function AdressPage() {
-  return (
-    <div className='flex flex-col sm:justify-center sm:items-center mb-72 px-3 sm:px-0'>
-      <div className='w-full  xl:w-[1000px] flex flex-col justify-center text-left'>
-        <Title title='Dirección' subtitle='Dirección de entrega' />
-        <AddressForm />
-      </div>
-    </div>
-  );
+export default async function AdressPage() {
+	const countries: Country[] = (await getCountries()) || [];
+
+	return (
+		<div className='flex flex-col sm:justify-center sm:items-center mb-72 px-3 sm:px-0'>
+			<div className='w-full  xl:w-[1000px] flex flex-col justify-center text-left'>
+				<Title
+					title='Dirección'
+					subtitle='Dirección de entrega'
+				/>
+				<AddressForm countries={countries} />
+			</div>
+		</div>
+	);
 }
--- src/app/(shop)/checkout/address/ui/AddressForm.tsx
+++ src/app/(shop)/checkout/address/ui/AddressForm.tsx
@@ -1,117 +1,161 @@
-"use client";
+'use client';
 
-import clsx from "clsx";
-import type { FC } from "react";
-import { useForm } from "react-hook-form";
+import { Country } from '@/interfaces/country.interface';
+import clsx from 'clsx';
+import type { FC } from 'react';
+import { useForm } from 'react-hook-form';
 
-interface AddressFormProps {}
+interface AddressFormProps {
+	countries: Country[] | [];
+}
 type FormInput = {
-  firstName: string;
-  lastName: string;
-  address: string;
-  address2?: string;
-  postalCode: string;
-  city: string;
-  country: string;
-  phone: string;
-  rememberAddress: boolean;
+	firstName: string;
+	lastName: string;
+	address: string;
+	address2?: string;
+	postalCode: string;
+	city: string;
+	country: string;
+	phone: string;
+	rememberAddress: boolean;
 };
 
-const AddressForm: FC<AddressFormProps> = () => {
-  const {
-    handleSubmit,
-    register,
-    formState: { isValid },
-  } = useForm<FormInput>({
-    defaultValues: {
-      // Todo : Obtener datos de la cuenta
-    },
-  });
+const AddressForm: FC<AddressFormProps> = ({ countries }) => {
+	const {
+		handleSubmit,
+		register,
+		formState: { isValid },
+	} = useForm<FormInput>({
+		defaultValues: {
+			// Todo : Obtener datos de la cuenta
+		},
+	});
 
-  const onSubmit = (data: FormInput) => {
-    console.log({ data });
-  };
-  return (
-    <form onSubmit={handleSubmit(onSubmit)} className='grid grid-cols-1 gap-2 sm:gap-5 sm:grid-cols-2'>
-      <div className='flex flex-col mb-2'>
-        <span>Nombres</span>
-        <input type='text' className='p-2 border rounded-md bg-gray-200/70' {...register("firstName", { required: true })} />
-      </div>
+	const onSubmit = (data: FormInput) => {
+		console.log({ data });
+	};
+	return (
+		<form
+			onSubmit={handleSubmit(onSubmit)}
+			className='grid grid-cols-1 gap-2 sm:gap-5 sm:grid-cols-2'>
+			<div className='flex flex-col mb-2'>
+				<span>Nombres</span>
+				<input
+					type='text'
+					className='p-2 border rounded-md bg-gray-200/70'
+					{...register('firstName', { required: true })}
+				/>
+			</div>
 
-      <div className='flex flex-col mb-2'>
-        <span>Apellidos</span>
-        <input type='text' className='p-2 border rounded-md bg-gray-200/70' {...register("lastName", { required: true })} />
-      </div>
+			<div className='flex flex-col mb-2'>
+				<span>Apellidos</span>
+				<input
+					type='text'
+					className='p-2 border rounded-md bg-gray-200/70'
+					{...register('lastName', { required: true })}
+				/>
+			</div>
 
-      <div className='flex flex-col mb-2'>
-        <span>Dirección</span>
-        <input type='text' className='p-2 border rounded-md bg-gray-200/70' {...register("address", { required: true })} />
-      </div>
+			<div className='flex flex-col mb-2'>
+				<span>Dirección</span>
+				<input
+					type='text'
+					className='p-2 border rounded-md bg-gray-200/70'
+					{...register('address', { required: true })}
+				/>
+			</div>
 
-      <div className='flex flex-col mb-2'>
-        <span>Dirección 2 (opcional)</span>
-        <input type='text' className='p-2 border rounded-md bg-gray-200/70' {...register("address2")} />
-      </div>
+			<div className='flex flex-col mb-2'>
+				<span>Dirección 2 (opcional)</span>
+				<input
+					type='text'
+					className='p-2 border rounded-md bg-gray-200/70'
+					{...register('address2')}
+				/>
+			</div>
 
-      <div className='flex flex-col mb-2'>
-        <span>Código postal</span>
-        <input type='text' className='p-2 border rounded-md bg-gray-200/70' {...register("postalCode", { required: true })} />
-      </div>
+			<div className='flex flex-col mb-2'>
+				<span>Código postal</span>
+				<input
+					type='text'
+					className='p-2 border rounded-md bg-gray-200/70'
+					{...register('postalCode', { required: true })}
+				/>
+			</div>
 
-      <div className='flex flex-col mb-2'>
-        <span>Ciudad</span>
-        <input type='text' className='p-2 border rounded-md bg-gray-200/70' {...register("city", { required: true })} />
-      </div>
+			<div className='flex flex-col mb-2'>
+				<span>Ciudad</span>
+				<input
+					type='text'
+					className='p-2 border rounded-md bg-gray-200/70'
+					{...register('city', { required: true })}
+				/>
+			</div>
 
-      <div className='flex flex-col mb-2'>
-        <span>País</span>
-        <select className='p-2 border rounded-md bg-gray-300/20' {...register("country", { required: true })}>
-          <option value=''>[ Seleccione ]</option>
-          <option value='CRI'>Costa Rica</option>
-        </select>
-      </div>
+			<div className='flex flex-col mb-2'>
+				<span>País</span>
+				<select
+					className='p-2 border rounded-md bg-gray-300/20'
+					{...register('country', { required: true })}>
+					<option value=''>[ Seleccione ]</option>
+					{countries.map((country) => (
+						<option
+							key={country.id}
+							value={country.name}>
+							{country.name}
+						</option>
+					))}
+				</select>
+			</div>
 
-      <div className='flex flex-col mb-2'>
-        <span>Teléfono</span>
-        <input type='text' className='p-2 border rounded-md bg-gray-300/20' {...register("phone", { required: true })} />
-      </div>
+			<div className='flex flex-col mb-2'>
+				<span>Teléfono</span>
+				<input
+					type='text'
+					className='p-2 border rounded-md bg-gray-300/20'
+					{...register('phone', { required: true })}
+				/>
+			</div>
 
-      <div className='flex flex-col mb-2 sm:mt-1'>
-        <div className='inline-flex items-center mb-10'>
-          <label
-            className='relative flex cursor-pointer items-center rounded-full p-3'
-            htmlFor='checkbox'
-            data-ripple-dark='true'>
-            <input
-              type='checkbox'
-              className="border-gray-500 before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-md border border-blue-gray-200 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-blue-500 checked:bg-blue-500 checked:before:bg-blue-500 hover:before:opacity-10"
-              id='checkbox'
-              {...register("rememberAddress")}
-            />
-            <div className='pointer-events-none absolute top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 text-white opacity-0 transition-opacity peer-checked:opacity-100'>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                className='h-3.5 w-3.5'
-                viewBox='0 0 20 20'
-                fill='currentColor'
-                stroke='currentColor'
-                strokeWidth='1'>
-                <path
-                  fillRule='evenodd'
-                  d='M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z'
-                  clipRule='evenodd'></path>
-              </svg>
-            </div>
-          </label>
-          <span>Recordar dirección?</span>
-        </div>
+			<div className='flex flex-col mb-2 sm:mt-1'>
+				<div className='inline-flex items-center mb-10'>
+					<label
+						className='relative flex cursor-pointer items-center rounded-full p-3'
+						htmlFor='checkbox'
+						data-ripple-dark='true'>
+						<input
+							type='checkbox'
+							className="border-gray-500 before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-md border border-blue-gray-200 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-blue-500 checked:bg-blue-500 checked:before:bg-blue-500 hover:before:opacity-10"
+							id='checkbox'
+							{...register('rememberAddress')}
+						/>
+						<div className='pointer-events-none absolute top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 text-white opacity-0 transition-opacity peer-checked:opacity-100'>
+							<svg
+								xmlns='http://www.w3.org/2000/svg'
+								className='h-3.5 w-3.5'
+								viewBox='0 0 20 20'
+								fill='currentColor'
+								stroke='currentColor'
+								strokeWidth='1'>
+								<path
+									fillRule='evenodd'
+									d='M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z'
+									clipRule='evenodd'></path>
+							</svg>
+						</div>
+					</label>
+					<span>Recordar dirección?</span>
+				</div>
 
-        <button disabled={!isValid} type='submit' className={clsx("btn-primary", !isValid && "btn-disabled")}>
-          Siguiente
-        </button>
-      </div>
-    </form>
-  );
+				<button
+					disabled={!isValid}
+					type='submit'
+					className={clsx('btn-primary', !isValid && 'btn-disabled')}>
+					Siguiente
+				</button>
+			</div>
+		</form>
+	);
 };
 
 export default AddressForm;
